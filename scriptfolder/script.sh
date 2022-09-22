61501f007105c16feb4b8a6addfc90802e71b3fc
EC2 Default User <ec2-user@ip-172-31-14-142.us-west-2.compute.internal>
2022-09-22 16:40:00 +0000

    ./script.sh

---
+++ scriptfolder/script.sh
@@ -0,0 +1,26 @@
+#! bin/bash
+
+function f1()
+{
+	top
+	echo "top"
+}
+function f2()
+{
+	df -H
+	echo "disk usage"
+}
+function f3()
+{
+	name=$1
+	echo "name"
+}
+function f4()
+{
+	sudo grep error var/log/messages
+	echo "last 10 messages shows"
+}
+f1
+f2
+f3
+f4
